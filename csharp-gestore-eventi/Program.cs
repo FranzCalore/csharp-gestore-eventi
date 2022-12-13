c3473a587b1517ecf2d10eb5b37e66a897d735c4
FranzCalore <franzkaiser1993@gmail.com>
2022-12-13 11:23:13 +0100

    Completata Milestone 4

--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -2,21 +2,59 @@
 
 using csharp_gestore_eventi;
 
-Console.WriteLine("Inserisci il titolo di un evento da inserire:");
-string titolo = Console.ReadLine();
-Console.WriteLine();
-Console.WriteLine("Inserisci la data dell'evento (dd/mm/yyyy)");
-string data = Console.ReadLine();
-Console.WriteLine();
-Console.WriteLine("Inserisci la capienza massima dell'evento");
-uint CapienzaEvento = uint.Parse(Console.ReadLine());
-Console.WriteLine();
-Evento Eventoutente = new Evento(titolo, data, CapienzaEvento);
-Console.WriteLine(Eventoutente);
-Console.WriteLine("Capienza Massima Evento: " + Eventoutente.GetCapienzaMassima());
-Console.WriteLine("Numero Posti Prenotati: " + Eventoutente.GetNumeroPostiPrenotati());
-Console.WriteLine();
-Console.WriteLine("Vuoi prenotare dei posti? (y/n)");
+
+
+Console.WriteLine("Inserisci il nome del tuo programma di eventi:");
+string titoloProgramma = Console.ReadLine();
+ProgrammaEventi nuovoProgrammaEventi = new ProgrammaEventi(titoloProgramma);
+Console.WriteLine("Quanti eventi vuoi che contenga il tuo programma?");
+uint numeroEventiDaProgrammare = uint.Parse(Console.ReadLine());
+
+while (nuovoProgrammaEventi.NumeroEventi() != numeroEventiDaProgrammare)
+{
+    try
+    {
+        Console.WriteLine("Inserisci il titolo di un evento da inserire:");
+        string titolo = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine("Inserisci la data dell'evento (dd/mm/yyyy)");
+        string data = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine("Inserisci la capienza massima dell'evento");
+        uint CapienzaEvento = uint.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Evento Eventoutente = new Evento(titolo, data, CapienzaEvento);
+        nuovoProgrammaEventi.AggiungiEvento(Eventoutente);
+        Console.WriteLine(Eventoutente);
+        Console.WriteLine("Capienza Massima Evento: " + Eventoutente.GetCapienzaMassima());
+        Console.WriteLine("Numero Posti Prenotati: " + Eventoutente.GetNumeroPostiPrenotati());
+        Console.WriteLine();
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine("La data inserita è in formato errato");
+    }
+    catch (OverflowException e)
+    {
+        Console.WriteLine("Non puoi inserire numeri negativi");
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+Console.WriteLine("Gli eventi programmati sono: " + nuovoProgrammaEventi.NumeroEventi());
+Console.WriteLine("Gli eventi programmati sono: " + nuovoProgrammaEventi.ToString());
+Console.WriteLine("Inserisci una data di cui vuoi visualizzare gli eventi (dd/mm/yyyy):");
+string dataUtente = Console.ReadLine();
+Console.WriteLine(ProgrammaEventi.StampaListaInConsole(nuovoProgrammaEventi.EventiInData(dataUtente)));
+nuovoProgrammaEventi.SvuotaListaEventi();
+
+
+
+
+
+/*Console.WriteLine("Vuoi prenotare dei posti? (y/n)");
 string InputUtente = Console.ReadLine();
 while (InputUtente == "y")
 {
@@ -52,3 +90,4 @@ while (InputUtente == "y")
 }
 Console.WriteLine();
 Console.WriteLine("Buona Giornata!");
+*/
--- csharp-gestore-eventi/ProgrammaEventi.cs
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -6,7 +6,67 @@ using System.Threading.Tasks;
 
 namespace csharp_gestore_eventi
 {
-    internal class ProgrammaEventi
+    public class ProgrammaEventi
     {
+        private string titolo;
+        private List<Evento> eventi;
+
+
+        public ProgrammaEventi(string titolo)
+        {
+            this.titolo = titolo;
+            this.eventi = new List<Evento>();
+        }
+
+        public string GetTitolo()
+        {
+            return this.titolo;
+        }
+
+        public void AggiungiEvento(Evento Evento)
+        {
+            this.eventi.Add(Evento);
+        }
+        public List<Evento> EventiInData(string stringData)
+        {
+            DateTime data = DateTime.Parse(stringData);
+            List<Evento> ListaEventiInData= new List<Evento>();
+            foreach(Evento evento in this.eventi)
+            {
+                if (evento.GetData() == data)
+                {
+                    ListaEventiInData.Add(evento);
+                }
+            }
+            return ListaEventiInData;
+        }
+
+        public static string StampaListaInConsole(List<Evento> ListaEventi)
+        {
+            string stringaEventi = "\n";
+            foreach (Evento evento in ListaEventi)
+            {
+                stringaEventi += evento.ToString();
+                stringaEventi += "\n";
+            }
+            return stringaEventi;
+        }
+
+        public int NumeroEventi()
+        {
+            return eventi.Count();
+        }
+
+        public void SvuotaListaEventi()
+        {
+            eventi.Clear();
+        }
+
+        public override string ToString()
+        {
+            string stringaListaEventi = this.GetTitolo() + ":\n";
+            stringaListaEventi += ProgrammaEventi.StampaListaInConsole(this.eventi);            
+            return stringaListaEventi;
+        }
     }
 }
